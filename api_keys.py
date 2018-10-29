45f270adbe84d114ddebba29bca59087edee5c7b
T McBeth <mcbethtyler@gmail.com>
2018-10-29 16:33:52 -0400

    Did stuff

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,8 @@
 # OpenWeatherMap API Key
-api_key = "YOUR KEY HERE!"
+api_key = "426009bcd2e301dbe0d258f4e81d28bf"
+
+#Dark Skys API
+darksky_api = "dc9475c5c90cc88a48c6196b3adf234a"
+
+#Google Maps API
+gkey = "AIzaSyBrR9OPKN3ug0EjtnwImWXSzZXPDwENjww"
